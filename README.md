10f1ccb769c4ac75edde1eb372c32d7c4c65a0e9
RozaTsy <ayara2015@mail.ru>
2023-02-08 14:45:19 +0300

    gendiff flat files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/roza-ts/python-project-50/workflows/hexlet-check/badge.svg)](https://github.com/roza-ts/python-project-50/actions)
+
+[![asciicast](https://asciinema.org/a/558234.svg)](https://asciinema.org/a/558234)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,5 @@
 import argparse
+from gendiff import generate_diff
 
 
 def main():
@@ -6,8 +7,13 @@ def main():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help="set format of output")
-    parser.parse_args()
-        
+    args = parser.parse_args() 
+    file1 = args.first_file
+    file2 = args.second_file
+    print(generate_diff(file1, file2)) 
+      
 
 if __name__ == '__main__':
     main()
+
+
